94388b010db71708e055bcdf027e4e53a01faaf7
qianqian512 <512657752@qq.com>
2019-06-13 22:45:04 +0800

    修改日志配置

--- src/main/java/org/hum/nettyproxy/ServerRun.java
+++ src/main/java/org/hum/nettyproxy/ServerRun.java
@@ -19,21 +19,15 @@ public class ServerRun {
 	 * <pre>
 	 *  墙内服务器(http-inside-server)启动命令:
 	 *    nettyproxy.runmode=11 nettyproxy.port=3389 nettyproxy.outside_proxy_host=127.0.0.1 nettyproxy.outside_proxy_port=5432 nettyproxy.workercnt=96
+	 *  墙内服务器(socks-inside-server)启动命令:
+	 *    nettyproxy.runmode=12 nettyproxy.port=3389 nettyproxy.outside_proxy_host=127.0.0.1 nettyproxy.outside_proxy_port=5432 nettyproxy.workercnt=96
 	 *  墙外服务器(outside-server)启动命令:
 	 *    nettyproxy.runmode=100 nettyproxy.port=5432 nettyproxy.workercnt=96
-	 *HTTP转发服务器(http-simple-server)启动命令:
+	 *	HTTP转发服务器(http-simple-server)启动命令:
 	 *     nettyproxy.runmode=1 nettyproxy.port=3389 nettyproxy.outside_proxy_host=127.0.0.1 nettyproxy.outside_proxy_port=5432 nettyproxy.workercnt=96
 	 * </pre> 
 	 */
 	public static void main(String[] args) {
-		// http-inside-server
-		// args = "nettyproxy.runmode=11 nettyproxy.port=3389 nettyproxy.outside_proxy_host=127.0.0.1 nettyproxy.outside_proxy_port=5432 nettyproxy.workercnt=96".split(" ");
-		// socks-inside-server
-		 args = "nettyproxy.runmode=12 nettyproxy.port=3389 nettyproxy.outside_proxy_host=127.0.0.1 nettyproxy.outside_proxy_port=5432 nettyproxy.workercnt=96".split(" ");
-		// http-simple-server
-		// args = "nettyproxy.runmode=1 nettyproxy.port=3389 nettyproxy.outside_proxy_host=127.0.0.1 nettyproxy.outside_proxy_port=5432 nettyproxy.workercnt=96".split(" ");
-		// outside-server
-//		 args = "nettyproxy.runmode=100 nettyproxy.port=5432 nettyproxy.workercnt=96".split(" ");
 		NettyProxyConfig serverRunArg = NettyProxyConfigParser.toServerRunArg(args);
 		logger.info("input_args=" + serverRunArg);
 		ServerRunProxyFactory.create(serverRunArg.getRunMode()).start(serverRunArg);
