483b81335c2f4c75646dabe5930abd64f3c47f1c
huming <512657752@qq.com>
2020-06-07 20:16:20 +0800

    证书创建还是有问题，改用别的代码创建证书试试

--- src/test/java/org/hum/nettyproxy/test/https_client/ca/impl/CA_Test.java
+++ src/test/java/org/hum/nettyproxy/test/https_client/ca/impl/CA_Test.java
@@ -2,17 +2,11 @@ package org.hum.nettyproxy.test.https_client.ca.impl;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.security.InvalidKeyException;
 import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.SecureRandom;
-import java.security.SignatureException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
@@ -30,104 +24,37 @@ import sun.security.x509.X509CertInfo;
 
 @SuppressWarnings("restriction")
 public class CA_Test {
-	
+
 	/**
 	 * 产生证书库，并创建CA 别名末认为CA
-	 * 
-	 * @param store     证书库
-	 * @param storePass 证书库密码
-	 * @param ca        ca信息
-	 * @param caPass    ca密码
 	 */
-	public static void createCA(File store, String storePass, X500Name ca, String caPass) {
+	public static void createCA(File outPath, String storePass, X500Name caSubject, String caPass) throws Exception {
 
 		// 设置证书密钥类型和签名类型
-		CertAndKeyGen cak = null;
-		try {
-			cak = new CertAndKeyGen("RSA", "SHA1withRSA", null);
-		} catch (NoSuchAlgorithmException | NoSuchProviderException e) {
-
-			e.printStackTrace();
-		}
+		CertAndKeyGen cak = new CertAndKeyGen("RSA", "SHA1withRSA", null);
 
 		// 设置安全随机数
-		SecureRandom secureRandom = null;
-		try {
-			secureRandom = SecureRandom.getInstance("SHA1PRNG", "SUN");
-		} catch (NoSuchAlgorithmException | NoSuchProviderException e) {
-
-			e.printStackTrace();
-		}
-
+		SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG", "SUN");
 		cak.setRandom(secureRandom);
-
-		// 产生随机的公私密钥对 1024 bits
-		try {
-			cak.generate(1024);
-		} catch (InvalidKeyException e) {
-
-			e.printStackTrace();
-		}
-
-		// 设置CA的信息
-		X500Name suject = ca;
+		cak.generate(1024);
 
 		// 产生一个自签名的证书
-		X509Certificate certificate = null;
-		try {
-			certificate = cak.getSelfCertificate(suject, new Date(), 50 * 365 * 24L * 60L * 60L);
-		} catch (InvalidKeyException | CertificateException | SignatureException | NoSuchAlgorithmException
-				| NoSuchProviderException e) {
-
-			e.printStackTrace();
-		}
+		X509Certificate certificate = cak.getSelfCertificate(caSubject, new Date(), 50 * 365 * 24L * 60L * 60L);
 		// 设置证书验证链
 		X509Certificate[] certs = { certificate };
 
 		// 将CA的别名，私钥，密码存入keystore中
-		KeyStore keyStore = null;
-		try {
-			keyStore = KeyStore.getInstance("JKS");
-		} catch (KeyStoreException e) {
+		KeyStore keyStore = KeyStore.getInstance("JKS");
+		keyStore.load(null, storePass == null ? null : storePass.toCharArray());
+		keyStore.setKeyEntry("CA", cak.getPrivateKey(), caPass.toCharArray(), certs);
 
-			e.printStackTrace();
-		}
-
-		try {
-			keyStore.load(null, storePass.toCharArray());
-		} catch (NoSuchAlgorithmException | CertificateException | IOException e) {
-
-			e.printStackTrace();
-		}
-
-		try {
-			keyStore.setKeyEntry("CA", cak.getPrivateKey(), caPass.toCharArray(), certs);
-		} catch (KeyStoreException e) {
-
-			e.printStackTrace();
-		}
-
-		FileOutputStream fos = null;
-		try {
-			fos = new FileOutputStream(store);
-		} catch (FileNotFoundException e) {
-
-			e.printStackTrace();
-		}
-
-		try {
-			keyStore.store(fos, storePass.toCharArray());
-		} catch (KeyStoreException | NoSuchAlgorithmException | CertificateException | IOException e) {
-
-			e.printStackTrace();
-		}
-
-		try {
-			fos.close();
-		} catch (IOException e) {
-
-			e.printStackTrace();
-		}
+		FileOutputStream fos = new FileOutputStream(outPath);
+		keyStore.store(fos, storePass == null ? null : storePass.toCharArray());
+		fos.close();
+	}
+	
+	public static void main(String[] args) throws IOException, Exception {
+		createCA(new File("/Users/hudaming/Workspace/GitHub/netty-proxy/src/test/java/org/hum/nettyproxy/test/officaldemo/ca_and_cert/myca/rootca/certs/rootca4j.cert.p12"), "123456", new X500Name("EMAILADDRESS=huming@163.com, CN=HumingCN, OU=HumingOU, O=HumingO, ST=HumingST, C=CN"), "123456");
 	}
 
 	/**
@@ -136,28 +63,27 @@ public class CA_Test {
 	 * @param info          主体条目信息
 	 * @param certAlias     条目别名 根据密钥库个数产生
 	 * @param subjectPasswd 主体密码 随机产生6位字符串
-	 * @param caStore         密钥库
-	 * @param caStorePass     密钥库密码
+	 * @param caStore       密钥库
+	 * @param caStorePass   密钥库密码
 	 * @param CAname        CA名称
 	 * @param CApass        CA密码
-	 * @throws IOException 
-	 * @throws CertificateException 
+	 * @throws IOException
+	 * @throws CertificateException
 	 */
 	public static void createSubjectCert(String certAlias, String subjectPasswd, File caStore, String caStorePass,
 			String CAname, String CApass) throws Exception {
 
 		// 加载证书库
-		KeyStore keyStore = KeyStore.getInstance("PKCS12");
-		keyStore.load(new FileInputStream(caStore), caStorePass.toCharArray());
+		KeyStore caKeyStore = KeyStore.getInstance("PKCS12");
+		caKeyStore.load(new FileInputStream(caStore), caStorePass.toCharArray());
 
-		// 获取ca证书
-		X509Certificate caCert = (X509Certificate) keyStore.getCertificate(CAname);
+		// 获取CA证书
+		X509Certificate caCert = (X509Certificate) caKeyStore.getCertificate(CAname);
 
-		// 有效期 30年
+		// 有效期30年
 		long validity = 20 * 365 * 24L * 60L * 60L;
 		Date firstDate = new Date();
 		Date lastDate = new Date(firstDate.getTime() + validity);
-
 		CertificateValidity interval = new CertificateValidity(firstDate, lastDate);
 
 		// 产生公私密钥对信息
@@ -190,11 +116,10 @@ public class CA_Test {
 		x509Info.set(X509CertInfo.VALIDITY, interval);
 
 		// 获取CA私钥
-		PrivateKey CAPrivateKey = (PrivateKey) keyStore.getKey(CAname, CApass.toCharArray());
+		PrivateKey CAPrivateKey = (PrivateKey) caKeyStore.getKey(CAname, CApass.toCharArray());
 
 		// 对subject签名
 		X509CertImpl cert = new X509CertImpl(x509Info);
-		
 		cert.sign(CAPrivateKey, "SHA1withRSA");
 
 		// 设置证书验证链
@@ -206,19 +131,19 @@ public class CA_Test {
 		certKeyStore.setKeyEntry(certAlias, certAndKeyGen.getPrivateKey(), subjectPasswd.toCharArray(), certs);
 
 		String outpath = "/Users/hudaming/Workspace/GitHub/netty-proxy/src/test/java/org/hum/nettyproxy/test/officaldemo/ca_and_cert/myca/rootca/dynamic/huming_test2.p12";
-		
+
 		System.out.println(new File(outpath).delete());
-		
+
 		FileOutputStream fos = new FileOutputStream(outpath);
 		certKeyStore.store(fos, caStorePass.toCharArray());
 		fos.close();
 	}
-	
+
 	/**
 	 * 目前通过Java生成的已经和用openssl生成的cert基本保持一致了（详细对比，只有有效期和序列号不同，其他关键信息例如签名算法，CA信息完全一致）
 	 * 但调用时仍然提示「javax.crypto.AEADBadTagException: Tag mismatch」错误
 	 */
-	public static void main(String[] args) throws Exception {
+	public static void main2(String[] args) throws Exception {
 		long start = System.currentTimeMillis();
 		File store = new File("/Users/hudaming/Workspace/GitHub/netty-proxy/src/test/java/org/hum/nettyproxy/test/officaldemo/ca_and_cert/myca/rootca/server_cert.p12");
 		createSubjectCert("1", "123456", store, "123456", "nickli", "123456");
