4257678ffa7654b5d8298afd7ddd51ba1f8e9fcf
huming <huming>
2020-07-25 07:51:49 +0800

    delete

--- src/test/java/org/hum/nettyproxy/test/https_client/ca/bouncycastle/_1/Store.java
+++
@@ -1,8 +0,0 @@
-package org.hum.nettyproxy.test.https_client.ca.bouncycastle._1;
-
-
-public interface Store<T> {
-	public void save(T obj, String password) throws Exception;
-
-	public T read() throws StorageException;
-}
--- src/test/java/org/hum/nettyproxy/test/ssl/test1/Readme.md
+++
@@ -1,3 +0,0 @@
-http://blog.sina.com.cn/s/blog_90ad2e8b0102x5wt.html
-
-https://my.oschina.net/ydsakyclguozi/blog/612886
