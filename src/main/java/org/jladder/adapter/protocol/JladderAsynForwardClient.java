be5add8b7d4ebc058987e51ba273adad300cc6d8
hudaming1 <huming@missfresh.cn>
2020-12-17 20:04:22 +0800

    修改bug

--- src/main/java/org/jladder/adapter/protocol/JladderAsynForwardClient.java
+++ src/main/java/org/jladder/adapter/protocol/JladderAsynForwardClient.java
@@ -106,14 +106,14 @@ public class JladderAsynForwardClient extends ChannelInboundHandlerAdapter {
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 		ByteBuf byteBuf = (ByteBuf) msg;
 		jladderAsynForwardClientInvokeChain.onReceiveData(new JladderByteBuf(byteBuf));
-        ctx.fireChannelRead(byteBuf.retain());
+//        ctx.fireChannelRead(byteBuf.retain());
 	}
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
     	log.info(this.channel.toString() + "(" + id + ")" + " diconnect");
     	jladderAsynForwardClientInvokeChain.onDisconnect(new JladderChannelHandlerContext(ctx));
-        ctx.fireChannelInactive();
+//        ctx.fireChannelInactive();
     }
     
     public void close() {
